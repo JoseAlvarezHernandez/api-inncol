557f81296cc1693ee7b1a20e4f56d1f3c18cae3d
Jose de Jesus Alvarez Hernandez <josedejesus.alvarez@broxel.com>
2018-02-14 12:15:36 -0600

    Updated API routes and added API documentation

---
+++ configs/swagger.js
@@ -0,0 +1,40 @@
+/**
+ * @module config/swagger
+ * @author Jose de Jesus Alvarez Hernandez
+ * @desc App Swagger specifications file
+ */
+
+/** Swagger JSDoc instance */
+const swaggerJSDoc = require('swagger-jsdoc');
+/** URL instance */
+const url = require('url');
+
+/** URL host */
+const urlHost = url.parse(process.env.api_url).host;
+/** API Port */
+const apiPort = process.env.port;
+
+const swaggerDefinition = {
+    info: {
+        title: 'API',
+        version: '1.0.0',
+        description: 'API Documentation',
+    },
+    host: (urlHost == 'localhost') ? (urlHost + ':' + apiPort) : urlHost,
+    basePath: '/',
+    // Order of tags in UI
+    tags: [],
+};
+
+/** Swagger options object */
+const options = {
+    // import swaggerDefinitions
+    swaggerDefinition: swaggerDefinition,
+    // path to the API docs
+    apis: ['./controllers/*.js'],
+};
+const specification = swaggerJSDoc(options);
+
+/** Swagger specification */
+module.exports = specification;
+
--- routes/pathTree.js
+++ routes/pathTree.js
@@ -4,6 +4,8 @@
  * @desc pathTree Routes
  */
 const users = require('./../controllers/users');
+const swaggerDoc = require('./../configs/swagger');
+const restify = require('restify');
 const pathTree = {
     middlewares: [
         {
@@ -13,39 +15,43 @@ const pathTree = {
     ],
     allowedMethods: {
         get: fakeController,
-        post: fakeController,
-        del: fakeController
     },
     subPaths: {
-        user: {
-            allowedMethods: {
-                get: users.getAll
-            },
+        api: {
             subPaths: {
-                ':userId': {
+                users: {
                     allowedMethods: {
-                        get: users.getUser,
-                        put: fakeController,
-                        del: fakeController,
-                        patch: fakeController,
+                        get: users.getAll
+                    },
+                    subPaths: {
+                        ':userId': {
+                            allowedMethods: {
+                                get: users.getUser,
+                                put: fakeController,
+                                del: fakeController,
+                                patch: fakeController,
+                            }
+                        }
+                    }
+                },
+                register: {
+                    allowedMethods: {
+                        get: fakeController,
+                        post: fakeController
                     }
                 }
             }
         },
-        register: {
-            allowedMethods: {
-                get: fakeController,
-                post: fakeController
-            }
-        }
-    },
+    }
 }
 function middleware(req, res, next) {
-    req.count = 1;
     return next()
 }
 function fakeController(req, res) {
-    req.count = (req.count) ? req.count + 1 : 0;
-    res.send(200, { count: req.count, path: req.route.path })
+    res.send(200, `Welcome to ${process.env.name}'s API`)
+}
+function swagger(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerDoc);
 }
 module.exports = pathTree;
