a4d973535a8bdca453c542aa4b3df5c9c4536b10
Jose de Jesus Alvarez Hernandez <alvarez_3993@hotmail.com>
2018-02-26 15:54:16 -0600

    Update crossOrigins.js
    Added options for cors

--- configs/crossOrigins.js
+++ configs/crossOrigins.js
@@ -1,6 +1,6 @@
 const crossOrigins = (request, response, next) => {
     response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    response.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, HEAD, PATCH');
     response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     return next();
 }
