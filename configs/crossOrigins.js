7a636118a036ef4528e29c0c59f4d444a4242e30
Jose de Jesus Alvarez Hernandez <alvarez_3993@hotmail.com>
2018-02-26 15:56:37 -0600

    Update crossOrigins.js

--- configs/crossOrigins.js
+++ configs/crossOrigins.js
@@ -1,7 +1,7 @@
 const crossOrigins = (request, response, next) => {
     response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, HEAD, PATCH');
-    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, source, Authorization');
     return next();
 }
 module.exports = crossOrigins;
